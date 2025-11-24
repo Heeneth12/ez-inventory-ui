cb0734f81aee4337d87de13265d2455a36395cde
Heeneth12 <heeneth123@gmail.com>
2025-11-24 08:32:16 +0530

    created base components

---
+++ src/app/views/sales/sales-returns/sales-returns.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-returns',
+  standalone: true,
+  imports: [],
+  templateUrl: './sales-returns.component.html',
+  styleUrl: './sales-returns.component.css'
+})
+export class SalesReturnsComponent {
+
+}
