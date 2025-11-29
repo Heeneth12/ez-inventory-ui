faffaf695ef6f9b0df9322c51af54b427d6f78fb
Heeneth12 <heeneth123@gmail.com>
2025-11-29 17:02:27 +0530

    add settings component with template, styles, and unit tests; update routes to include settings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { ForbiddenComponent } from './views/forbidden/forbidden.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { ItemsRoutes } from './views/items/items.routes';
-import { InventoryRoutes } from './views/inventory/inventory.routes';
 import { PurchasesRoutes } from './views/purchases/purchases.routes';
 import { SalesRoutes } from './views/sales/sales.routes';
 import { ReportsRoutes } from './views/reports/reports.routes';
@@ -10,17 +9,23 @@ import { DocumentsRoutes } from './views/documents/documents.routes';
 import { AuthComponent } from './views/auth/auth.component';
 import { UserManagementComponent } from './views/user-management/user-management.component';
 import { contactsRoutes } from './views/contacts/contacts.routes';
+import { stockRoutes } from './views/stock/stock.routes';
+import { EmployeeComponent } from './views/employee/employee.component';
+import { SettingsComponent } from './views/settings/settings.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'items', children: ItemsRoutes },
-    { path: 'inventory', children: InventoryRoutes },
+    { path: 'stock', children: stockRoutes },
     { path: 'purchases', children: PurchasesRoutes },
     { path: 'sales', children: SalesRoutes },
     { path: 'reports', children: ReportsRoutes },
-    { path: 'contacts', children:contactsRoutes },
+    { path: 'contacts', children: contactsRoutes },
+    { path: 'employee', component: EmployeeComponent },
     { path: 'documents', children: DocumentsRoutes },
+    { path: 'settings', component: SettingsComponent },
+    // auth routes
     { path: 'auth', component: AuthComponent },
     // admin routes
     { path: 'admin/user-management', component: UserManagementComponent },
