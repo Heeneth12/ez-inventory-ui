67fec43756f7f97114d356a642c348549ca73c6f
Heeneth12 <heeneth123@gmail.com>
2025-11-29 17:04:23 +0530

    add sales order management components including form, service, modal, and routing; implement UI and functionality for creating and managing sales orders

--- src/app/views/sales/sales.routes.ts
+++ src/app/views/sales/sales.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { SalesComponent } from './sales.component';
+import { InvoicesComponent } from './invoices/invoices.component';
+import { SalesOrderRoutes } from './sales-order/sales-order.router';
 
 
 export const SalesRoutes: Routes = [
     { path: '', component: SalesComponent },
+    { path: 'order', children: SalesOrderRoutes },
+    { path: 'invoices', component: InvoicesComponent }
 ];
