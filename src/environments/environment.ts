112140cd3918cecb45798e3f8d2a44c4e428e090
Heeneth12 <heeneth123@gmail.com>
2026-01-01 16:07:12 +0530

    fix: passing vm arge for versal

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  authUrl: (window as any).__env?.AUTH_URL || 'http://localhost:8080',
-  devUrl: (window as any).__env?.DEV_URL || 'http://localhost:8085',
+  authUrl: (window as any).__env?.AUTH_URL,
+  devUrl: (window as any).__env?.DEV_URL,
   appName: 'Inventory',
   appKey: 'EZH_INV_001',
 };
