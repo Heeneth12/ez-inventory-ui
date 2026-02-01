154049bc213082993eb16425dbd70acaf76a249c
Heeneth12 <heeneth123@gmail.com>
2026-02-01 10:55:29 +0530

    feat: create PRQ Purchase request mobule

--- src/app/views/purchases/purchase.routes.ts
+++ src/app/views/purchases/purchase.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { PurchasesOrderRoutes } from './purchase-order/purchase-order.routes';
 import { GoodsReceiptRoutes } from './goods-receipt/purchase-return.routes';
 import { PurchaseReturnRoutes } from './purchase-returns/purchase-return.routes';
+import { PurchasesRequestRoutes } from './purchase-request/purchase-request.routes';
 
 
 export const PurchasesRoutes: Routes = [
+    { path: 'prq', children: PurchasesRequestRoutes },
     { path: 'order', children: PurchasesOrderRoutes },
     { path: 'grn', children: GoodsReceiptRoutes },
     { path: 'return', children: PurchaseReturnRoutes },
