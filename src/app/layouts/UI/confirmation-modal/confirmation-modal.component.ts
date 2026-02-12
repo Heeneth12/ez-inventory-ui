297b551073066f1970cb29739c5b99e2ff380c74
heeneth123 <heeneth123@gmail.com>
2026-02-12 10:24:32 +0000

    feat: added tailwind elements and imple in confirm modal

--- src/app/layouts/UI/confirmation-modal/confirmation-modal.component.ts
+++ src/app/layouts/UI/confirmation-modal/confirmation-modal.component.ts
@@ -1,27 +1,50 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  effect,
+  CUSTOM_ELEMENTS_SCHEMA
+} from '@angular/core';
 import { ConfirmationModalService, ModalIntent } from './confirmation-modal.service';
 import { CircleX, Info, LucideAngularModule, Settings2, Trash2 } from 'lucide-angular';
+import '@tailwindplus/elements';
 
 @Component({
   selector: 'app-confirmation-modal',
   standalone: true,
   imports: [CommonModule, LucideAngularModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './confirmation-modal.component.html',
   styleUrl: './confirmation-modal.component.css'
 })
 export class ConfirmationModalComponent {
-  constructor(public modalSvc: ConfirmationModalService) {}
 
-  //icon 
+  // Reference to the native dialog element
+  @ViewChild('confirmDialog') dialogRef!: ElementRef<HTMLDialogElement>;
+
+  // Icons
   readonly CircleX = CircleX;
   readonly Trash2 = Trash2;
   readonly Settings2 = Settings2;
   readonly Info = Info;
 
-  /**
-   * Returns appropriate Material Icon string based on modal intent
-   */
+  constructor(public modalSvc: ConfirmationModalService) {
+    // Watch the signal: Open/Close dialog automatically
+    effect(() => {
+      const isOpen = !!this.modalSvc.activeModal();
+      if (this.dialogRef?.nativeElement) {
+        if (isOpen) {
+          if (!this.dialogRef.nativeElement.open) {
+            this.dialogRef.nativeElement.showModal();
+          }
+        } else {
+          this.dialogRef.nativeElement.close();
+        }
+      }
+    });
+  }
+
   getIcon(intent: ModalIntent): any {
     switch (intent) {
       case 'danger': return this.Trash2;
