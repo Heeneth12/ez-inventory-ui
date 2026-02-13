db8aee6193636c9e4df8942d05d5a459b69461a1
Heeneth12 <heeneth123@gmail.com>
2026-02-13 21:22:36 +0530

    feat: added  new component for ASN and pr for vender base code

---
+++ src/app/views/vendor/asn/asn.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asn',
+  standalone: true,
+  imports: [],
+  templateUrl: './asn.component.html',
+  styleUrl: './asn.component.css'
+})
+export class AsnComponent {
+
+}
---
+++ src/app/views/vendor/v-purchase-return/v-purchase-return.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-v-purchase-return',
+  standalone: true,
+  imports: [],
+  templateUrl: './v-purchase-return.component.html',
+  styleUrl: './v-purchase-return.component.css'
+})
+export class VPurchaseReturnComponent {
+
+}
